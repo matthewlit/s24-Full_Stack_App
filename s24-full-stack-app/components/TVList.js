004f8ae76a09ccf8568507310479da58071e3884
Matthew Kelleher <Matthew.kelleher10@comcast.net>
2024-02-25 20:32:14 -0500

    Added streaming platform data from TMDB

--- s24-full-stack-app/components/TVList.js
+++ s24-full-stack-app/components/TVList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Colors from "../library/Colors";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import infoHandler from "@/pages/api/getInfo";
 
 // Display list of shows or movies
 const TVList = ({ data, emptyMessage = "Error" }) => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [providers, setProviders] = useState([]);
 
   const openPopup = (item) => {
     setSelectedItem(item);
@@ -15,32 +17,69 @@ const TVList = ({ data, emptyMessage = "Error" }) => {
     setSelectedItem(null);
   };
 
+  useEffect(() => {
+    const getProviders = async () => {
+      const request =
+        "https://api.themoviedb.org/3/tv/" +
+        selectedItem.id +
+        "/watch/providers";
+      const data = await infoHandler(request);
+      console.log(data);
+      setProviders(data.results);
+      console.log(providers);
+    };
+
+    if (selectedItem != null) getProviders();
+  }, [selectedItem]);
+
   console.log(data);
 
   return (
     <Container>
-      {data.length === 0 ? 
-        <NoItem>{emptyMessage}</NoItem> : 
+      {data.length === 0 ? (
+        <NoItem>{emptyMessage}</NoItem>
+      ) : (
         data.map((item) => (
           <Item key={item.id}>
-            <Image src={'http://image.tmdb.org/t/p/w500' + item.poster_path} />
+            <Image src={"http://image.tmdb.org/t/p/w500" + item.poster_path} />
             <NavButton onClick={() => openPopup(item)}>More Info</NavButton>
           </Item>
         ))
-      }
+      )}
       {selectedItem && (
         <Popup>
           <PopupContent>
-            <BackDropImage src={'http://image.tmdb.org/t/p/w500' + selectedItem.backdrop_path} />
+            <BackDropImage
+              src={
+                "http://image.tmdb.org/t/p/w500" + selectedItem.backdrop_path
+              }
+            />
             <InfoWrapper>
               <Title>Title:</Title>
-                <PopupText>{selectedItem.title ? selectedItem.title : selectedItem.name}</PopupText>
+              <PopupText>
+                {selectedItem.title ? selectedItem.title : selectedItem.name}
+              </PopupText>
               <Title>Description:</Title>
-                <PopupText>{selectedItem.overview}</PopupText>
+              <PopupText>{selectedItem.overview}</PopupText>
               <Title>Rating:</Title>
-                <PopupText>{Math.round(selectedItem.vote_average*10)/10 + '/10'}</PopupText>
-              <Title>Streaming Platforms:</Title>
-                <PopupText>{selectedItem.platforms}</PopupText>
+              <PopupText>
+                {Math.round(selectedItem.vote_average * 10) / 10 + "/10"}
+              </PopupText>
+              {providers.US != undefined ? (
+                <>
+                  <Title>Streaming Platforms:</Title>
+                  <LogoWrapper>
+                    {providers.US.flatrate.map((item) => (
+                      <Logo
+                        key={item.provider_id}
+                        src={"http://image.tmdb.org/t/p/w500" + item.logo_path}
+                      />
+                    ))}
+                  </LogoWrapper>
+                </>
+              ) : (
+                <></>
+              )}
             </InfoWrapper>
             <NavButton onClick={closePopup}>Close</NavButton>
           </PopupContent>
@@ -55,26 +94,26 @@ const Container = styled.div`
   flex-direction: row;
   background-color: ${Colors.secondary};
   overflow: auto;
-`
+`;
 
 const Item = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-right: 2.5vw;
-`
+`;
 
 const Image = styled.img`
   width: 10vw;
   height: 15vw;
   border-radius: 1vw;
-`
+`;
 
 const Title = styled.h3`
   margin: 1vw;
   font-size: 1vw;
   font-weight: bold;
-`
+`;
 
 const NoItem = styled.h2`
   display: flex;
@@ -83,7 +122,7 @@ const NoItem = styled.h2`
   align-items: center;
   margin: 2.5vw;
   justify-content: center;
-`
+`;
 
 const NavButton = styled.button`
   background-color: ${Colors.accentDark};
@@ -101,7 +140,7 @@ const NavButton = styled.button`
   &:hover {
     background-color: ${Colors.accentLight};
   }
-`
+`;
 
 const Popup = styled.div`
   position: fixed;
@@ -114,7 +153,7 @@ const Popup = styled.div`
   justify-content: center;
   align-items: center;
   overflow: auto;
-`
+`;
 
 const PopupContent = styled.div`
   display: flex;
@@ -126,24 +165,44 @@ const PopupContent = styled.div`
   width: 30%;
   margin-top: 5vw;
   margin-bottom: 1vw;
-`
+`;
 
 const InfoWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin: 1vw;
-`
+  width: 100%;
+  margin-left: 5vw;
+  margin-right: 5vw;
+`;
 
 const PopupText = styled.p`
   font-size: 1vw;
   margin-left: 2vw;
-`
+`;
 
 const BackDropImage = styled.img`
   width: 75%;
   border-radius: 1vw;
   border-style: solid;
   box-shadow: 2px 2px 5px ${Colors.accentLight};
-`
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 2vw;
+  margin-right: 2vw;
+  width: 100%;
+  overflow: auto;
+`;
+
+const Logo = styled.img`
+  width: 2.5vw;
+  border-radius: 0.5vw;
+  box-shadow: 2px 2px 5px ${Colors.accentLight};
+  margin-bottom: 1vw;
+  margin-right: 1vw;
+`;
 
 export default TVList;
